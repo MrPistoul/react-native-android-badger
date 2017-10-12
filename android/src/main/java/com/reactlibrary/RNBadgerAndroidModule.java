9d9f50269fa781cf6ced0618f54539823bf22f57
sebastien <seb.dangelo@gmail.com>
2017-10-12 09:44:57 +0200

    Project 1.0

---
+++ android/src/main/java/com/reactlibrary/RNBadgerAndroidModule.java
@@ -0,0 +1,30 @@
+
+package com.reactlibrary;
+
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.Callback;
+
+public class RNBadgerAndroidModule extends ReactContextBaseJavaModule {
+
+  private final ReactApplicationContext reactContext;
+
+  public RNBadgerAndroidModule(ReactApplicationContext reactContext) {
+    super(reactContext);
+    this.reactContext = reactContext;
+  }
+
+  @Override
+  public String getName() {
+    return "RNBadgerAndroid";
+  }
+  @ReactMethod
+  public void setBadge(int badgeCount) {
+    ShortcutBadger.applyCount(reactContext, badgeCount)
+  }
+  @ReactMethod
+  public void removeBadge() {
+    ShortcutBadger.removeCount(reactContext);
+  }
+}
---
+++ index.js
@@ -0,0 +1,6 @@
+
+import { NativeModules } from 'react-native';
+
+const { RNBadgerAndroid } = NativeModules;
+
+export default RNBadgerAndroid;
